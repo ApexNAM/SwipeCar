94b029a3169350a6577f3d4c53d4cb3d350c263f
ApexNAM <46587415+ApexNAM@users.noreply.github.com>
2021-03-24 14:53:04 +0900

    스와프카 제작 완료

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text distance;
+
+    private GameObject player;
+    private GameObject flag;
+
+
+    void Start()
+    {
+        this.player = GameObject.FindGameObjectWithTag("Player");
+        this.flag = GameObject.FindGameObjectWithTag("Flag");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float length = this.flag.transform.position.x - this.player.transform.position.x;
+
+        if (length > 0.5f)
+            distance.text = "목표 지점까지 " + length.ToString("F2") + "m";
+        else if (length <= 0.5f && length >= -0.5f)
+            distance.text = "승리했다!";
+        else
+            distance.text = "gaMeover";
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float moveSpeed = 0.0f;
+
+    AudioSource audioSource;
+    Vector2 startPos;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPos = transform.position;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = (endPos.x - startPos.x);
+
+            this.moveSpeed = swipeLength / 500.0f;
+            audioSource.Play();
+        }
+
+        moveSpeed *= 0.98f;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(Vector2.right * moveSpeed, ForceMode2D.Impulse);
+    }
+}
